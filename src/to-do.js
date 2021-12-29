6a7911e14ea2b9afe067a42d15e5a63268e8fd30
Acer <buddha6772@gmail.com>
2021-12-30 08:42:18 +1100

    initial commit

---
+++ src/to-do.js
@@ -0,0 +1,30 @@
+import React from 'react'
+class TODO extends React.Component{
+    constructor(props){
+        super(props)
+        this.state={
+            text:''
+        }
+        this.handeChange=this.handeChange.bind(this)
+    }
+    handeChange(e){
+        this.setState({text: e.target.value})
+    }
+       
+    
+    render(){
+        return(
+<div>
+    <h1>TODO APPLICATION</h1>
+    <form>
+        <label htmlFor='id'>Input Items</label>
+        <label>{this.state.text}</label><br/>
+        <input id='id' onChange={this.handeChange} value={this.state.text}/>
+        <button>Add</button>
+    </form>
+</div>
+        )
+        
+    }
+}
+export default TODO
